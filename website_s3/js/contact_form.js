c18201ed41edb4f029981241e7fbbf3c63467e6d
LevinOX <daniel.wrede@posteo.de>
2023-05-10 22:03:01 +0200

    add js function for contact form APIgw request

---
+++ website_s3/js/contact_form.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('contact-form').addEventListener('submit', submitContactForm);
+});
+
+
+function submitContactForm(event) {
+    event.preventDefault();
+
+    const apiUrl = 'https://jk348hof93.execute-api.eu-central-1.amazonaws.com/chat_api_stage/your-resource';
+
+    const name = document.getElementById('cname').value;
+    const email = document.getElementById('cemail').value;
+    const message = document.getElementById('cmessage').value;
+
+    $.ajax({
+        url: apiUrl,
+        type: 'POST',
+        data: `name=${encodeURIComponent(name)}&email=${encodeURIComponent(email)}&message=${encodeURIComponent(message)}`,
+        contentType: 'application/x-www-form-urlencoded; charset=utf-8',
+        dataType: 'json',
+        success: function (response) {
+            console.log('Message sent successfully:', response);
+            // You can add any additional logic here, e.g., display a success message or reset the form
+        },
+        error: function (error) {
+            console.error('Error:', error);
+            // You can add any additional logic here, e.g., display an error message
+        }
+    });
+}
